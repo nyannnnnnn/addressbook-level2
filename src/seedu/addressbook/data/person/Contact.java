d8d36001f8986fabc1cb5d1efe4d428970eb492d
nyannnnnnn <yannuoyuan@gmail.com>
2016-09-07 20:51:50 +0800

    Update Contact.java

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -1,14 +1,33 @@
 package seedu.addressbook.data.person;
 
 import seedu.addressbook.data.exception.IllegalValueException;
+
 public class Contact{
-    private String Value;
-    
-    public Contact(String contact){
- 		  this.Value = contact;
+    private String value;
+    private boolean isPrivate;
+    public Contact(String contact, boolean isPrivate, String validation, String constrains){
+ 		this.isPrivate = isPrivate;
+        contact = contact.trim();
+        if (!isValid(contact, validation)) {
+            throw new IllegalValueException(constrains);
+        }
+        this.value = contact;
+ 	  }
+ 	  
+ 	  private boolean isValid(String contact, String validation){
+ 	      return test.mathes(contact, validation);
+ 	  }
+ 	  
+ 	  public boolean isPrivate(){
+ 	      return isPrivate;
  	  }
  	  
  	  public String toString(){
  	    return Value;
+ 	  }
+ 	  
+ 	  @Override
+ 	  public int hashCode() {
+ 	    return value.hashCode();
  	  }
  }
